28ce369200dab01ca15f6bec23c0e3271105199a
Syed Aawaiz Ashraf <syedaawaiz49@gmail.com>
2023-12-04 01:54:10 +0300

    master -01

---
+++ sidePan.js
@@ -0,0 +1,8 @@
+<html>
+  <body>
+    <h2>JS main page</h2>
+
+    <p id="main"> JavaScript chages</p>
+    <button type="button" onClick='document.getElementById("main").innerHTML = "Hey user Welcom!"'>Submit</button>
+  </body>
+</html>
